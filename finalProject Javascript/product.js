cb14e82e3843a07aff93a6ec1b1932d55fc1f9e1
Pragatti-harchand <pragatti.harchand@green-apex.com>
2021-08-31 20:48:41 +0530

    final project

---
+++ finalProject Javascript/product.js
@@ -0,0 +1,16 @@
+const array = [{
+  title: "Leather Watch",
+  description: "Colorful Watch. This simple quartz watch is riotous with color and eye-catching, full of vitality and youth.",
+  img: "../image/download.jpg"
+}, {
+  title: "Hot Fashion Lovely watch",
+  description: "Cheap Women's Watches, Buy Quality Watches Directly from China",
+  img: "../image/teenager.jpg"
+}, {
+  title: "watch relogio masulino female",
+  description: "Women's Watch Simple Fashion Luxury Teenage Girl Wristwatch lovely ...",
+  img: "../image/wrist.jpg"
+}];
+
+
+
