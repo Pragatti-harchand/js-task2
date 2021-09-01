a87bef2c39693bef22f9b13370f98a69a4c2161d
Pragatti-harchand <pragatti.harchand@green-apex.com>
2021-09-01 22:34:47 +0530

    final project

--- finalProject Javascript/product.js
+++ finalProject Javascript/product.js
@@ -1,16 +1,43 @@
-const array = [{
-  title: "Leather Watch",
-  description: "Colorful Watch. This simple quartz watch is riotous with color and eye-catching, full of vitality and youth.",
-  img: "../image/download.jpg"
-}, {
-  title: "Hot Fashion Lovely watch",
-  description: "Cheap Women's Watches, Buy Quality Watches Directly from China",
-  img: "../image/teenager.jpg"
-}, {
-  title: "watch relogio masulino female",
-  description: "Women's Watch Simple Fashion Luxury Teenage Girl Wristwatch lovely ...",
-  img: "../image/wrist.jpg"
-}];
+
+var titlearray = ["css", "js", "python", "java", "android", "jquery"];
+var descriptionarray = ["css style", "js program", "python code", "java objects", "android program", "jquery objects"];
+var dynamic = document.querySelector('.container');
+for (var i = 0; i < titlearray.length; i++) {
+    var fetch = document.querySelector('.container').innerHTML;
+    dynamic.innerHTML = `<div id="cards${i}" class="boxes">
+
+
+
+
+
+    <div class="box-content">
+    <h5>${titlearray[i]}</h5>
+    <p>
+    ${descriptionarray[i]}
+    </p>
+    <a class="showmore" href="#">
+    </a>
+    <i class="fa fa-thumbs-up"></i>
+    <i class="fa fa-edit"></i>
+    <i class="fa fa-trash"></i>
+    </div>
+    </div>` + fetch;
+
+    var bgimg = document.getElementById(`cards${i}`);
+
+    bgimg.style.backgroundImage = `url('../image/${titlearray[i]}.jpg')`;
+
+}
+itemSearch(titlearray)
+function itemSearch(titlearray) {
+    var filter = document.getElementById('myInput').value;
+    for (var i = 0; i < titlearray.length; i++) {
+
+        var fetch = document.querySelector('.container').innerHTML;
+
+    }
+}
+
 
 
 
