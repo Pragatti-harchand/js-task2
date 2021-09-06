3010ebf2233ebdc4f6dd434f22e49f032e4c79c8
Pragatti-harchand <pragatti.harchand@green-apex.com>
2021-09-06 16:54:50 +0530

    deleted

--- tipcalculator-7-8-21/cal.js
+++
@@ -1,8 +0,0 @@
-let billAmount=prompt("enter the bill amont");
-let bill=parseInt(billAmount);
-console.log(`Bill Amount: ${billAmount}`);
-let tip=(bill<300)?bill*15/100:bill*20/100;
-console.log( `tip:${tip}`);
- let totalBill=tip+bill;
-console.log(`totalBill:${totalBill}`);
-
